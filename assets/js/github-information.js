a7ade5125408d662659f731ce4260c1c60d1a64b
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-01-19 20:19:44 +0000

    Add a 403 error message with reset timer to the Github API

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGithubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
 
     if (!username) {
@@ -60,8 +63,11 @@ function fetchGithubInformation(event) {
             $("#gh-user-data").html(userInformationHTML(userData));
             $("gh-repo-data").html(repoInformationHTML(repoData))
         }, function(errorResponse) {
-            if (errorResponse.status === "404") {
+            if (errorResponse.status === 404) {
                 $("gh-user-data").html('<h2>No info found for user ${username}</h2>');
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString("en-US")}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
@@ -70,3 +76,5 @@ function fetchGithubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGithubInformation());
